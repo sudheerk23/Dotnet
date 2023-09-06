d15a00d4e6ea69243b1d3f8f38511d80c7dc852c
sudheer <sudheerkurumeti1@gmail.com>
2023-09-06 22:55:54 +0530

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,12 +11,17 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
+
+
+// added by me
+builder.Services.AddCors();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
